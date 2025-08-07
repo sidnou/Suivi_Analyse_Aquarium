0dd353906096f90e106d9c3b4d3f4d9352b4c4c4
sidnou <sidjack972@gmail.com>
2025-08-07 22:58:15 +0200

    Création de l'application Web Suivi Analyse Aquarium Création des tables

--- analyse_aquarium/models.py
+++ analyse_aquarium/models.py
@@ -1,16 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
 class Aquarium(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
+
     def __str__(self):
         return f" {self.name} {self.quantity}"
+
 
 class Produits(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
-class Analyse(models.Model):
+
+
+class Robinet(models.Model):
+    quantity_robinet = models.IntegerField(help_text="En %")
+
+    class Meta:
+        abstract = True
+
+
+class Osmose(models.Model):
+    quantity_osmose = models.IntegerField(help_text="En %")
+
+    class Meta:
+        abstract = True
+
+
+class RemplacementEau(Robinet, Osmose):
+    quantity_remplacement_eau = models.IntegerField(help_text="En Litre")
+
+
+class Analyse(RemplacementEau):
     aquarium = models.ForeignKey(Aquarium, on_delete=models.CASCADE)
     nitrite = models.IntegerField(help_text="NO2")
     nitrate = models.IntegerField(help_text="N03")
@@ -23,11 +46,3 @@ class Analyse(models.Model):
     co2 = models.IntegerField(help_text="CO2")
     air = models.BooleanField(help_text="Air")
     produits = models.ForeignKey(Produits, on_delete=models.CASCADE)
-
-
-
-
-
-
-
-
