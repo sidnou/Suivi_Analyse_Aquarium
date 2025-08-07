78432d4acd69160cced016f8fb2fdfacffb84484
jasid <sidjack972@gmail.com>
2025-08-07 16:02:52 +0200

    Création de l'application Web Suivi Analyse Aquarium

---
+++ analyse_aquarium/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AnalyseAquariumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'analyse_aquarium'
---
+++ suivi_analyse_aquarium/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for suivi_analyse_aquarium project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suivi_analyse_aquarium.settings')
+
+application = get_wsgi_application()
