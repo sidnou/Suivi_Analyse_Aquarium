9953b64d8f90a679e2ff8e3aad5bf0bbeff9d578
jasid <sidjack972@gmail.com>
2025-08-13 16:54:20 +0200

    Création de l'application Web Suivi Analyse Aquarium Création des tables Test de la base de donnée: OK Exemple chat avec LLM local

--- analyse_aquarium/views.py
+++ analyse_aquarium/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+import ollama
+import environ
 
 # Create your views here.
+env = environ.Env()
+
+
+def chat_ollama_remote(request):
+    response_text = ""
+    prompt = ""
+
+    # ollama_api_url = env("LLM_LOCAL")
+    client = ollama.Client(host="") # exemple: "http://127.0.0.14:11434"
+    print(client.list())
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt', '')
+        if prompt:
+            try:
+                # Appel au modèle 'mistral' sur le serveur distant
+                # Assurez-vous que le modèle 'mistral' a été téléchargé sur le serveur distant
+                stream = client.chat(
+                    model='mistral-nemo',
+                    messages=[{'role': 'user', 'content': prompt}],
+                    stream=True
+                )
+
+                # Récupération de la réponse
+                full_response = ""
+                for chunk in stream:
+                    full_response += chunk['message']['content']
+
+                response_text = full_response
+            except Exception as e:
+                response_text = f"Erreur lors de la communication avec le serveur Ollama : {e}"
+
+    return render(request, 'analyse_aquarium/chat.html', {'response': response_text,
+        'prompt': prompt
+                                                          })
--- suivi_analyse_aquarium/settings.py
+++ suivi_analyse_aquarium/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'suivi_analyse_aquarium.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'analyse_aquarium/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
